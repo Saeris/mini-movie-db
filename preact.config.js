efe4434363ae1d9fb65435c5971806f4f88740dd
Drake Costa <Saeris@comcast.net>
2018-03-29 02:44:36 -0700

    Update preact-cli Config, Fix Build Script

---
+++ preact.config.js
@@ -0,0 +1,36 @@
+const { join, resolve } = require(`path`) // eslint-disable-line
+import CopyWebpackPlugin from "copy-webpack-plugin"
+import MinifyPlugin from "babel-minify-webpack-plugin"
+
+const pubDir = join(__dirname, `public`)
+
+export default (config, env, helpers) => {
+  if (env.isProd) {
+    const { index } = helpers.getPluginsByName(config, `UglifyJsPlugin`)[0]
+    config.plugins.splice(index, 1)
+    config.plugins.push(new MinifyPlugin({
+      keepFnName: false,
+      keepClassName: false,
+      booleans: true,
+      deadcode: true,
+      evaluate: true,
+      flipComparisons: true,
+      mangle: true,
+      memberExpressions: true,
+      mergeVars: true,
+      numericLiterals: true,
+      propertyLiterals: true,
+      removeConsole: true,
+      removeDebugger: true,
+      simplify: true,
+      simplifyComparisons: true,
+      typeConstructors: true,
+      undefinedToVoid: true
+    }, { sourceMap: null }))
+  }
+  config.plugins.push(new CopyWebpackPlugin([
+    { from: join(pubDir, `_redirects`) },
+    //{ from: join(pubDir, `favicon.ico`), to: `favicon.ico` },
+    { context: join(pubDir, `images`), from: `**/*`, to: `images` }
+  ]))
+}
