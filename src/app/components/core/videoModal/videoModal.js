30e76f8615c09783cc36eaad3c1cc87555a91890
Drake Costa <Saeris@comcast.net>
2018-03-31 17:14:28 -0700

    Fix Build Error for videoModal Component
    Had to remove `babel-plugin-transform-react-constant-elements` from `preact-cli`'s babel options in `preact.config/js`, because CodeSandbox uses a default babel config for preact projects and babel will throw an error if both `.babelrc` and `.babelrc.js` are present in a project.
    
    Added `yarn build` to `.travis.yml`'s script build step to ensure that builds of the app are successful before moving on to deployment. This should cause Travis builds to fail if this sort of error happens again.
    
    TODO: rework release process such that commits are never made directly to master, instead have them go through Travis first to run tests and ensure each step passes before mering any release code into master.

--- src/app/components/core/videoModal/videoModal.js
+++ src/app/components/core/videoModal/videoModal.js
@@ -1,27 +1,30 @@
 import FontAwesomeIcon from "@fortawesome/react-fontawesome"
 import { faPlay, faTimes } from "@fortawesome/fontawesome-free-solid"
 import YouTube from "react-youtube"
-import { Modal } from "../../structural"
+import { Modal } from "../../structural/modal"
 import "./videoModal.scss"
 
-export const VideoModal = ({ id, title, buttonText = `Play Video` }) => (
+export const VideoModal = ({ videoId, name, buttonText, ...props }) => (
   <Modal>
-    {({ isOpen, CloseModal, ModalContents, ModalOverlay, OpenModal }) => (
-      <div className="trailer">
+    {({ isOpen, OpenModal, ModalOverlay, ModalContent, CloseModal }) => (
+      <div>
         <OpenModal>
           <FontAwesomeIcon icon={faPlay} size="1x" />
           {` ${buttonText}`}
         </OpenModal>
+
         <ModalOverlay>
-          <ModalContents>
-            <div className="header">
-              <h1>{title}</h1>
-              <CloseModal>
-                <FontAwesomeIcon icon={faTimes} size="1x" />
-              </CloseModal>
-            </div>
-            {isOpen && <YouTube videoId={id} />}
-          </ModalContents>
+          {isOpen ? (
+            <ModalContent>
+              <div className="header">
+                <h1>{name}</h1>
+                <CloseModal>
+                  <FontAwesomeIcon icon={faTimes} size="1x" />
+                </CloseModal>
+              </div>
+              <YouTube videoId={videoId} />
+            </ModalContent>
+          ) : null}
         </ModalOverlay>
       </div>
     )}
