6547b0e4285df006cc997dc038de89f378cb5c1d
Drake Costa <drake@saeris.io>
2018-10-17 19:33:18 -0700

    🔧 Fix Broken Header Image Link

--- src/app/components/structural/navigation/navigation.js
+++ src/app/components/structural/navigation/navigation.js
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom"
 import routes from "../../../routes/routes"
 import "./navigation.scss"
 
-const logo = `https://www.themoviedb.org/static_cache/v4/logos/powered-by-square-green-11c0c7f8e03c4f44aa54d5e91d9531aa9860a9161c49f5fa741b730c5b21a1f2.svg`
+const logo = `https://www.themoviedb.org/assets/1/v4/logos/powered-by-square-green-11c0c7f8e03c4f44aa54d5e91d9531aa9860a9161c49f5fa741b730c5b21a1f2.svg`
 
 export const Navigation = () => (
   <ul className="navigation">
